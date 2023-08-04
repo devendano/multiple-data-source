3a3aed485d7e8676137aa2112af7baa6419eafe2
devendano <dev.avendano@gmail.com>
2023-08-04 16:39:05 -0400

    Change to rest controller

--- src/main/java/com/example/multipledatasource/controller/UserController.java
+++ src/main/java/com/example/multipledatasource/controller/UserController.java
@@ -1,29 +1,29 @@
 package com.example.multipledatasource.controller;
 
+import com.example.multipledatasource.dao.UserDaoImpl;
 import com.example.multipledatasource.model.User;
-import com.example.multipledatasource.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
     @Autowired
-    private UserService userService;
+    private UserDaoImpl userDao;
 
     @GetMapping("/all")
     public ResponseEntity<List<User>> getAllUsers() {
         List<User> users = new ArrayList<>();
         try {
-            users = userService.getAllUser();
+            users = userDao.getAllUsers();
         } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<>(users, HttpStatus.INTERNAL_SERVER_ERROR);
